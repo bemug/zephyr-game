8f51aacba84d11b40e4fbfecd24e9ea7e019c7bb
Benjamin Mugnier <mugnier.benjamin@gmail.com>
2015-02-20 16:13:34 +0100

    Add player movement support

--- src/Game.h
+++ src/Game.h
@@ -10,12 +10,14 @@ class Game
 		Game();
 		void run();
 	private:
+		void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
 		void processEvents();
 		void update();
 		void render();
 	private:
 		sf::RenderWindow mWindow;
 		sf::CircleShape mPlayer;
+		bool mIsMovingUp, mIsMovingDown, mIsMovingLeft, mIsMovingRight;
 };
 
 #endif
