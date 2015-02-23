9967cf240d05aebfb808c389beb4189d06e23724
Benjamin Mugnier <mugnier.benjamin@gmail.com>
2015-02-23 13:59:26 +0100

    Adapt resource holder for shaders handling

--- src/ResourceHolder.h
+++ src/ResourceHolder.h
@@ -16,6 +16,8 @@ class ResourceHolder
 {
 	public:
 		void load(Identifier id, const std::string& filename);
+		template <typename Parameter>
+		void load(Identifier id, const std::string& filename, const Parameter& secondparam);
 		Resource& get(Identifier id);
 		const Resource& get(Identifier id) const;
 	private:
