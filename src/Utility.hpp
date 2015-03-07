d6fa081f7198d85a993b5e87d3d6b5b5a5eef07e
Benjamin Mugnier <mugnier.benjamin@gmail.com>
2015-03-07 16:51:08 +0100

    Add utility functions

---
+++ src/Utility.hpp
@@ -0,0 +1,10 @@
+#ifndef _UTILITY_H
+#define _UTILITY_H
+
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Text.hpp>
+
+void centerOrigin(sf::Sprite& sprite);
+void centerOrigin(sf::Text& text);
+
+#endif
